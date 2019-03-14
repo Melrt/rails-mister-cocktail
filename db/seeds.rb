0084cb8c52786c87a41fe23f56cd3d74c2107370
melanie rat <RAT.melanie@gmail.com>
2019-03-14 15:41:10 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning cocktails + doses database...'
+Cocktail.destroy_all
+
+puts 'Cleaning ingredient'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+10.times do
+  ingredient = Ingredient.new(
+    name: Faker::Food.fruits
+  )
+  ingredient.save!
+end
+
+10.times do
+  ingredient = Ingredient.new(
+    name: Faker::Food.spice
+  )
+  ingredient.save!
+end
+
+puts 'Creating cocktails...'
+
+5.times do
+  # Créer un cocktail vide
+  cocktail = Cocktail.new(
+    name: Faker::Ancient.primordial
+  )
+  cocktail.save!
+
+  # Choisir 3 ingrédients
+  ingredients = Ingredient.all
+  cocktail_ingredients = ingredients.sample(3)
+
+  # Créer 3 doses de ces ingrédients pour mon cocktail
+  doses = cocktail_ingredients.map do |ingredient|
+    Dose.create!(
+      cocktail: cocktail,
+      ingredient: ingredient,
+      description: Faker::Food.measurement,
+    )
+  end
+  # => [<Active# ingredient 1,,>, <Activ# ingredient 2...>]
+end
+
+puts 'Finished!'
+
